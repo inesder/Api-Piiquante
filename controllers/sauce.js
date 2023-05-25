fa2fd51a9ab4a83951051b092202b4091874c636
Inès Derraz <i.derraz@my-goodlife.fr>
2023-05-25 16:24:53 +0200

    add get route

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -8,8 +8,11 @@ exports.createSauce = (req, res, next) => {
     const sauce = new Sauce({
         ...sauceObject,
         userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes:0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: []        });
     sauce.save()
     .then(
         () => {
@@ -20,6 +23,22 @@ exports.createSauce = (req, res, next) => {
     ).catch(
         (error) => {
             res.status(400).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({
+        _id: req.params.id
+    }).then(
+        (sauce) => {
+            res.status(200).json(sauce);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
                 error: error
             });
         }
